eed7c2548486ad6935a65bf78a21f1abac88d563
Ana-Maria <anamariadumitrache2004@gmai.com>
2025-11-08 08:49:37 +0000

    [week9]: COMPOSITE PATTERN - Leaf (MenuItem)

---
+++ src/main/java/com/cafepos/menu/MenuItem.java
@@ -0,0 +1,32 @@
+package com.cafepos.menu;
+import com.cafepos.common.Money;
+import java.util.Collections;
+import java.util.Iterator;
+
+//Leaf nodes in the Composite Design Pattern -  represent actual menu items; they don’t need to support child manipulation
+
+public final class MenuItem extends MenuComponent {
+    private final String name;
+    private final Money price;
+    private final boolean vegetarian;
+
+    public MenuItem(String name, Money price, boolean vegetarian) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("name required");
+        if (price == null) throw new IllegalArgumentException("price required");
+        this.name = name;
+        this.price = price;
+        this.vegetarian = vegetarian;
+    }
+
+    @Override public String name() { return name; }
+    @Override public Money price() { return price; }
+    @Override public boolean vegetarian() { return vegetarian; }
+    @Override public Iterator<MenuComponent> iterator() {
+        return Collections.emptyIterator();
+    }
+    @Override public void print() {
+        String veg = vegetarian ? " (V)" : "";
+        System.out.println(" - " + name + veg + " = " + price);
+    }
+}
